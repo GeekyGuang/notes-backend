b4558fd7c9f7386dff4a8952e3d88e3785c45227
TravisWongX <travis.x.wong@gmail.com>
2021-07-22 17:32:38 +0800

    get date from mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const { request, response } = require('express')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(express.json())
 app.use(cors())
@@ -38,34 +40,26 @@ let notes = [
     }
 ]
 
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-    console.log(body)
 
     if (!body.content) {
         return response.status(400).json({
@@ -73,16 +67,16 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId()
-    }
+    })
 
-    notes = notes.concat(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 
-    response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
