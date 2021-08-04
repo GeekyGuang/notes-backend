56008c52835c2cba1f70abde18307f4bac914ce4
TravisWongX <travis.x.wong@gmail.com>
2021-08-04 09:50:59 +0800

    populate

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,9 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('notes', { content: 1, date: 1 })
   response.json(users)
 })
 
